4f71273e5febb241d4798033160fefd53b8b327a
Emmanuel <emmanuel_aviles@Ubuntu.myguest.virtualbox.org>
2023-12-02 17:56:33 -0600

    Crear logica del codigo

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -11,19 +11,27 @@ class MonitorProceso:
 
     # Funcion encargada de llevar a cabo el proceso
     def ejecutar_proceso(self):
-
-
-
+       try:
+            subprocess.Popen(self.comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            print(f"Proceso {self.nombre_proceso} iniciado.")
+        except Exception as e:
+            print(f"Error al iniciar el proceso {self.nombre_proceso}: {e}")
 
 
 
     # Funcion encargada de monitorear el proceso
     def monitorear_proceso(self):
 
+        # Verifica si el proceso está en ejecución consultando la lista de procesos del sistema
+        while True:
+            proceso_en_ejecucion = any(self.nombre_proceso in linea for linea in os.popen('tasklist'))
+            
+            # Si el proceso se ha cerrado, imprime un mensaje y reinicia el proceso
+            if not proceso_en_ejecucion:
+                print(f"¡Atención! El proceso {self.nombre_proceso} ha sido cerrado. Reiniciando...")
+                self.ejecutar_proceso()
 
-
-
-
+            time.sleep(5)  # Delay de 5 segundos
 
 
 
