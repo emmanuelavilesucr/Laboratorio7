e9a85c3017c3e7ef0a3d409f181e0410e011962e
Emmanuel <emmanuel_aviles@Ubuntu.myguest.virtualbox.org>
2023-12-02 14:52:14 -0600

    Correccion del argumento de la funcion obtener_informacion_proceso()

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -2,7 +2,7 @@ import psutil
 import sys
 
 # Esta funcion es encargada de obtener todos los datos relacionados al proceso. 
-def obtener_informacion_proceso():
+def obtener_informacion_proceso(pid):
     try:
     proceso = psutil.Process(pid)   # Obtiene el objeto del proceso utilizando psutil
 
