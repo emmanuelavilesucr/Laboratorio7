b9b290fe11add7d41a6e59f075666d1c6b44c4ce
Emmanuel <emmanuel_aviles@Ubuntu.myguest.virtualbox.org>
2023-12-02 15:23:33 -0600

    Agregar estructura del codigo

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -1,4 +1,37 @@
 import os
 import subprocess  
 import time 
-import sys  
+import sys 
+
+# Clase del Proceso
+class MonitorProceso:
+    def __init__(self, nombre_proceso, comando):
+        self.nombre_proceso = nombre_proceso
+        self.comando = comando
+
+    # Funcion encargada de llevar a cabo el proceso
+    def ejecutar_proceso(self):
+
+
+
+
+
+
+    # Funcion encargada de monitorear el proceso
+    def monitorear_proceso(self):
+
+
+
+
+
+
+
+
+# Funcion del main
+def main():
+
+
+
+
+
+main()
