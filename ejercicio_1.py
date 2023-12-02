867189b153092f0365f1c0c0d06f0009de033e83
Emmanuel <emmanuel_aviles@Ubuntu.myguest.virtualbox.org>
2023-12-02 12:46:51 -0600

    Estructura o funciones a utilizar en el codigo

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,3 +1,17 @@
 import psutil
 import sys
 
+# Esta funcion es encargada de obtener todos los datos relacionados al proceso. 
+def obtener_informacion_proceso():
+
+
+
+
+
+# Funcion Main.
+def main():
+
+
+
+
+main()
